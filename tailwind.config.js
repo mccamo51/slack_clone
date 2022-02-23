ab9901ddc63e925c907eba8b3daf4b76f646affc
Bismark <mccamo51@gmail.com>
2022-02-23 17:12:06 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        HeaderBG: "#350D36",
+        seachBG: "#3F0E40"
+      }
+    },
+  },
+  plugins: [],
+}
