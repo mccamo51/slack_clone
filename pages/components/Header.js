4fe6f968c859e1f3220f564ce10d1b723c5f10ae
Bismark <mccamo51@gmail.com>
2022-03-01 03:11:29 +0000

    updade

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -3,14 +3,14 @@ import { Avatar, IconButton } from "@mui/material";
 import React from "react";
 
 function Header() {
-  return <div className="w-fill bg-HeaderBG h-[40px] flex ">
+  return <div className="w-fill bg-HeaderBG h-[40px] flex">
       <div className="flex-[0.99] flex justify-center ml-5">
       <IconButton>
           <FieldTimeOutlined style={{ color: "white" ,fontSize: "20px"}}/>
       </IconButton>
-     <div className="w-1/3 bg-seachBG mr-4 ml-4  mt-[7px] mb-[7px]  rounded-lg border-slate-700 border-[0.1px] flex justify-center items-center">
+     <div className="w-1/3 bg-seachBG mr-4 ml-4  mt-[7px] mb-[7px]  rounded-lg flex justify-center items-center">
          {/* <SearchOutlined style={{ color: "white" ,fontSize: "13px"}}/> */}
-     <input placeholder="🔍 Search slack theme" className=" border-none px-2
+     <input placeholder="🔍 Search slack theme" className="border-none px-2
       outline-none placeholder:text-slate-400 bg-seachBG
       placeholder:text-xs text-white placeholder:text-center text-center">
       </input>
--- pages/components/Sidebar.js
+++ pages/components/Sidebar.js
@@ -1,13 +1,14 @@
-import { IconButton } from '@mui/material'
+import { Avatar, IconButton } from '@mui/material'
 import React from 'react'
 import SidebarPersonButton from './SidebarPersonButton'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { CiCircleOutlined } from '@ant-design/icons/lib/icons';
 
 function Sidebar() {
     return (
         <div className='flex bg-red-400 h-screen '>
             <div className='flex-[0.3] flex bg-black'>
-                <div className='flex flex-[0.15] bg-leftSideBar border-r-[0.1px] border-r-slate-200/20 items-center flex-col'>
+                <div className='flex flex-[0.15] bg-leftSideBar border-r-[0.1px] border-r-slate-200/20 items-center flex-col border-t-[0.1px] border-t-slate-200/20'>
                     <SidebarPersonButton name="BA" selected={true} />
                     <SidebarPersonButton name="DA" selected={false} />
                     <IconButton className='text-white'>
@@ -15,18 +16,28 @@ function Sidebar() {
                     </IconButton>
 
                 </div>
-                <div className='flex flex-[0.85] bg-leftSideBar '>
+                <div className='flex flex-[0.85] bg-leftSideBar'>
                     <div className='h-[60px] w-full bg-leftSideBar p-2 border-y-[0.1px] border-y-slate-200/20'>
-                        <div className='flex '>
-                            <h4 className='text-white font-bold'>
-                                Gitplus.
-                            </h4>
-                            <KeyboardArrowDownIcon style={{ color: 'white' }} />
+                        <div className='flex justify-between items-center'>
+                            <div>
+                                <div className='flex '>
+                                    <h4 className='text-white font-bold'>
+                                        Gitplus.
+                                    </h4>
+                                    <KeyboardArrowDownIcon style={{ color: 'white' }} />
+                                </div>
+                                <div className='flex items-center'>
+                                    <CiCircleOutlined style={{ color: 'white' }} />
+                                    <h3 className='text-white font-light ml-2'>
+                                        Kelvin Ninson
+                                    </h3>
+                                </div>
+                            </div>
+                            <Avatar />
+
                         </div>
-                        <h3 className='text-white font-light'>
-                            Kelvin Ninson
-                        </h3>
                     </div>
+
                 </div>
             </div>
             <div className='flex-[0.7] flex bg-slate-600'>
